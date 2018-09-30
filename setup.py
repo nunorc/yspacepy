24e2dc4201899f2d66b54103159a67175d1f3f97
Nuno Carvalho <mestre.smash@gmail.com>
2018-09-30 13:44:19 +0100

    Initial import

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(name='yspacepy',
+      version='0.1',
+      description='http://y-space.pw',
+      url='https://github.com/y-space/yspacepy',
+      author='Nuno Carvalho',
+      author_email='narcarvalho@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
+
